ffed0c638d781de8b9d8738e102e30b58e83e0cd
ntebis <ntebis@gmail.com>
2025-06-15 07:40:14 +0000

    feat: Added user and table creation functions

--- backend/tests/test_database.py
+++ backend/tests/test_database.py
@@ -1,6 +1,34 @@
 import unittest
 from todo_backend.database import Database
+import random
 
-class TestDatabase(unittest):
+SET_REQUIRED_TABLES = {"users", "todos"}
 
-    def setUp():
+
+class TestDatabase(unittest.TestCase):
+
+    def setUp(self):
+        self.db = Database()
+
+
+    def test_tables(self):
+        # check if the tables are getting created correctly 
+        self.db.cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        tables = [row[0] for row in self.db.cursor.fetchall()]
+        # with sets the <= operator is the equivalent to issubset
+        self.assertLessEqual(SET_REQUIRED_TABLES, set(tables))
+
+    def test_users(self):
+        user_ids = []
+        usernames = []
+        # Testing if users are added as expected
+        num_users = 5
+        for i in range(num_users):
+            usernames.append(f"Andrew{i}")
+            user_ids.append(self.db.create_user(usernames[-1]))
+        # Asserting if the users created correctly 
+        self.assertEqual(len(set(user_ids)), num_users)
+        
+        # Asserting that if the user exists the function wil return -1
+        result = self.db.create_user(usernames[-1])
+        self.assertEqual(result, -1)
--- backend/todo_backend/database.py
+++ backend/todo_backend/database.py
@@ -1,8 +1,7 @@
 import sqlite3
-
+import logging
 
 class Database:
-    SET_REQUIRED_TABLES = {"users", "todos"}
 
     def __init__(self, database_file_location: str = None):
         """
@@ -14,15 +13,15 @@ class Database:
         self.db_conn = sqlite3.connect(database_file_location) if database_file_location else sqlite3.connect("database.db") 
         self.cursor = self.db_conn.cursor()
 
-        self.create_tables()
+        self._create_tables()
 
-    def create_tables(self) -> None:
+    def _create_tables(self) -> None:
         """Creates the tables if they do not exists"""
 
         create_users_table = """
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
+            username TEXT NOT NULL UNIQUE
         );
         """
         self.cursor.execute(create_users_table)
@@ -37,6 +36,32 @@ class Database:
         """
         self.cursor.execute(create_todos_table)
 
-# TODO: REMOVE ME
-if __name__ == "__main__":
-    db = Database()
+    def create_user(self, username: str) -> int:
+        """
+        Creates a user in the database
+
+        Args:
+            username (str): The username we want to add
+
+        Returns:
+            int: The user id of the user created or -1 if the username exists
+        """
+        try:
+            # adding user
+            add_user_sql = "INSERT INTO users (username) VALUES (?);"
+            self.cursor.execute(add_user_sql,(username.lower(),))
+            self.db_conn.commit()
+
+            #getting the id that was created
+            get_user_id = "SELECT id FROM users WHERE username = ?;"
+            self.cursor.execute(get_user_id, (username.lower(),))
+            user_id = self.cursor.execute(get_user_id, (username.lower(),)).fetchone()[0]
+            logging.info(f"User `{username}` was successfully create with the user id of {user_id}.")
+            return user_id
+        except sqlite3.IntegrityError:
+            logging.error(f"{username} exists. Try different username")
+            return -1
+
+
+            
+
