b780c66be06d01a5ed55ecc21a076b8387ceaf82
ntebis <ntebis@gmail.com>
2025-06-22 09:56:20 +0000

    feat: added typescript and html for todo items

--- frontend/src/app/components/todo-item/todo-item.ts
+++ frontend/src/app/components/todo-item/todo-item.ts
@@ -1,11 +1,80 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal, NgModule } from '@angular/core';
+import { TodoModel } from '../../models/todo.model';
+import { HttpClient } from '@angular/common/http';
+import { UserService } from '../../services/user-service';
+
+import { TodoService } from '../../services/todo-service';
+import { FormsModule } from '@angular/forms';
+import { Todos } from '../../todos/todos';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-todo-item',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './todo-item.html',
   styleUrl: './todo-item.css'
 })
 export class TodoItem {
-  todo = input.required
+
+  todo = input.required<TodoModel>();
+  isEditing: boolean = false;
+  editedTitle = signal('');
+  editedBody = signal('');
+  // editedTitle = '';
+  // editedBody = '';
+
+  constructor(
+    private http: HttpClient,
+    private userService: UserService, 
+    private todoService: TodoService, 
+    private todosComponent: Todos) { };
+
+  
+  /**
+   * Function that edits note
+   */
+  onEditClick(): void {
+
+    this.isEditing = true;
+    this.editedTitle = signal(this.todo().title);
+    this.editedBody = signal(this.todo().body);
+    // this.editedTitle = this.todo().title;
+    // this.editedBody = this.todo().body;
+    console.log('Edit mode entered for:', this.todo());
+  }
+
+
+  /**
+   * Function to update the note 
+   */
+  onSaveClick(): void {
+
+
+    // console.log('Saving changes:', updatedTodo);
+    this.todoService.editTodo(this.todo().id, this.editedTitle(), this.editedBody()).pipe(
+          catchError((err) => {
+            console.log(err);
+            throw err;
+          })).subscribe(() => {
+        console.log(`Edited note with id: ${this.todo().id}`)
+        this.todosComponent.loadTodos(this.userService._current_user!.id)
+        this.isEditing = false;
+      })
+  }
+
+  onCancelClick(): void {
+    this.isEditing = false;
+  }
+
+  onClickDelete() {
+    this.todoService.deleteTodo(this.todo().id).pipe(
+      catchError((err) => {
+            console.log(err);
+            throw err;
+          })
+    ).subscribe(() => {
+      console.log(`Removed note with id: ${this.todo().id}`)
+      this.todosComponent.loadTodos(this.userService._current_user!.id)
+    })
+  }
 }
